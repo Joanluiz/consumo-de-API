d0a4415b10b75bc68bf5a97fda6cf66f5093dc0f
Joan Luiz <84800919+Joanluiz@users.noreply.github.com>
2022-08-16 19:21:49 -0300

    adicionando lista de filmes

--- src/Services/movies.js
+++ src/Services/movies.js
@@ -1,46 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios'
 
-
 const FilmesApi = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/movie/550?api_key=52460cb453458e84f6b577c430c6eaf8'
+    baseURL:'https://api.themoviedb.org/3/movie/popular?api_key=52460cb453458e84f6b577c430c6eaf8&language=pt-BR&page=1'
 })
 
+
 class Movies extends Component {
-  state = {
+state={
     movies:[]
-  }
+}
 
-  componentDidMount() {
+componentDidMount(){
     this.getMovies()
-  }
+}
 
 
-  getMovies = async () => {
-    const resposta = await FilmesApi.get()
+getMovies = async () => {
+  const resposta = await FilmesApi.get();
+  console.log(resposta)
+  const AllFilmes = resposta.data.results.map((item) => {
+    return {
+      ...item,
+      nome:item.original_title
+    }
+  })
 
-    const AllFilmes = resposta.data.results.map((item)  => {
-      return {
-        nome:item.original_title,
-        sinopse:item.overview
-      }
-    })
-    this.setState({
-      movies:AllFilmes
-    })
-  }
+  this.setState({
+    movies: AllFilmes
+  }); console.log(this.state.movies)
+}
 
   render() {
     return (
-      <div>
-        {this.state.movies.map((item) => (
-          <ul>
-            <li>{item.nome}</li>
-            <li>{item.overview}</li>
-          </ul>
-        ))}
-      </div>
-    );
+        <div>
+            {this.state.movies.map((item) => (
+            <ul>
+              <li>{item.nome}</li>
+            </ul>
+            ))}
+        </div>
+      );
   }
 }
 
